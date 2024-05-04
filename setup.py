9ea6522e8ee98234229aa949f1b820aedce1f4ff
Hemant Sah <hemantsah18@gmail.com>
2024-05-04 16:12:25 +0530

    fixed install using pip

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import pathlib
 import setuptools
-import resmushit
 
 setuptools.setup(
     name="resmushit",
-    version=resmushit.__version__,
+    version="1.0.1",
     description="A wrapper for resmush.it API",
     long_description=pathlib.Path("README.md").read_text(encoding='utf-8'),
     long_description_content_type="text/markdown",
     url="https://resmush.it/",
-    author=resmushit.__author__,
+    author="Hemant Sah",
     author_email="hemantsah18@gmail.com",
     maintainer="Hemant Sah",
     download_url="https://github.com/hemansah/resmushit",
-    license=resmushit.__license__,
+    license="MIT",
     project_urls={
         "Homepage":"https://resmush.it/",
         "Documentation":"https://github.com/hemansah/resmushit/blob/master/README.md",
@@ -36,7 +35,6 @@ setuptools.setup(
     ],
     python_requires=">=3.6,<=3.12",
     install_requires=['urllib3', 'filetype'],
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
-    # test_requires=['pytest']
 )
